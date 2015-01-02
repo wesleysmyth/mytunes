9740a01f7edc3496b459de632598fe15b1413897
Wes <wesleytatesmith@gmail.com>
2015-01-02 00:01:52 -0800

    creates SongQueueEntryViews for each queued song & renders them

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,18 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
-  initialize: function() {
-  },
+  tagName: "table",
+
+  initialize: function(params) {
+    this.render();
+  }, 
 
   render: function() {
-    return this.$el;
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
--- test/spec/SongQueueViewSpec.js
+++ test/spec/SongQueueViewSpec.js
@@ -16,7 +16,7 @@ describe('SongQueueView', function() {
     ]);
   });
 
-  xit('creates SongQueueEntryViews for each queued song & renders them', function(){
+  it('creates SongQueueEntryViews for each queued song & renders them', function(){
     sinon.spy(SongQueueEntryView.prototype, 'render');
     view = new SongQueueView({collection: fakeSongs});
     view.render();
